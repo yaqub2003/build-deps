e0387bf87473690598273d2fb18ff659e65d0385
yaqub2003 <rasulov.yaqub@mail.ru>
2025-10-29 11:47:50 +0400

    Add dxva2 support for Windows in ffmpeg configuration

--- cmake/ffmpeg/ffmpeg.cmake
+++ cmake/ffmpeg/ffmpeg.cmake
@@ -97,6 +97,7 @@ endif()
 # OS specific options not defined by the above BUILD_FFMPEG_* options
 if(WIN32)
     list(APPEND FFMPEG_EXTRA_CONFIGURE
+            --enable-dxva2
             --enable-d3d11va
             --enable-encoder=h264_qsv,hevc_qsv,av1_qsv
             --enable-libvpl
